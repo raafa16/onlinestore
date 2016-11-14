a5c652be8837fea2a869e1a4f435e61510177c26
raafa16 <raafa16@hotmail.com>
2016-11-14 09:58:59 +0600

    logo changed

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   post 'product/cart'
   get 'product/contact'
 
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'product#index'
